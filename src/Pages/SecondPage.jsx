2367e9e0b99bdf8baa5064f56b2641e75404908f
saurabhrk92 <61101382+saurabhrk92@users.noreply.github.com>
2023-11-04 16:59:52 -0700

    Update SecondPage.jsx

--- src/Pages/SecondPage.jsx
+++ src/Pages/SecondPage.jsx
@@ -5,7 +5,7 @@ import imageone from "./../assests/image1.png";
 import imagetwo from "./../assests/image2.png";
 import imagethree from "./../assests/image3.png";
 import logo from "./../assests/KenmoreLogoDark.png";
-
+function SecondPage() {
   return (
     <div className="containerpagetwo">
       <div className="contanerinpage3">
