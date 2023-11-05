664f5e4c20bd93ab8641b55ea937189f52f03a6c
saurabhrk92 <61101382+saurabhrk92@users.noreply.github.com>
2023-11-04 18:36:47 -0700

    Update SecondPage.jsx

--- src/Pages/SecondPage.jsx
+++ src/Pages/SecondPage.jsx
@@ -11,7 +11,7 @@ function SecondPage() {
    async function checkTrue() {
     if (await verify()) {
       window.location.assign(
-        "https://appliance-demo1-pegjdiesz-saurabhrk92s-projects.vercel.app/PP-3"
+        "https://appliance-demo1.vercel.app/PP-3"
       );
     }
   }
