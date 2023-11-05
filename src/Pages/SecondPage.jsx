9371fa5ec2bc66c265558c2278e27bb0bb085286
saurabhrk92 <61101382+saurabhrk92@users.noreply.github.com>
2023-11-04 17:11:57 -0700

    Update SecondPage.jsx

--- src/Pages/SecondPage.jsx
+++ src/Pages/SecondPage.jsx
@@ -5,7 +5,6 @@ import imageone from "./../assests/image1.png";
 import imagetwo from "./../assests/image2.png";
 import imagethree from "./../assests/image3.png";
 import logo from "./../assests/KenmoreLogoDark.png";
-import * as tga from "https://engagetest.zblocks.io/token-gated/tga-verifier.js";
 
 function SecondPage() {
    async function checkTrue() {
